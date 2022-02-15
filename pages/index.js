75e26817798b3eb39284b7dc203f49d282ac8467
wonderwarrior <cahall555@gmail.com>
2022-02-14 21:45:22 -0700

    update for build.

--- pages/index.js
+++ pages/index.js
@@ -14,59 +14,59 @@ export default function Home() {
       </Head> 
       <div className="grid-rows-4" data-theme="cupcake">
 
-        <div class="hero min-h-screen" styles="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
-        <div class="hero-overlay bg-opacity-60"></div>
-        <div class="text-center hero-content text-neutral-content">
-          <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">Hello, I'm Courtney, aspiring full-stack engineer</h1>
-            <p class="mb-5">I am working on my BS in Computer Science. </p>
-            <button class="btn btn-primary">See Projects</button>
+        <div className="hero min-h-screen" styles="background-image: url();">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="text-center hero-content text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello</h1>
+            <p className="mb-5">I am working on my BS in Computer Science. </p>
+            <button className="btn btn-primary">See Projects</button>
           </div>
         </div>
       </div>
 
-      <div class="divider"></div> 
-        <div class="grid h-200 card bg-base-300 rounded-box place-items-center p-8">
-          <div class="grid grid-cols-3 gap-4">
-            <h2 class="col-span-1 text-slate-500" >- Work Experience</h2>
-            <h3 class="font-bold">Product Manager | Data Integration Manager</h3>
-            <h5 class="col-end-4 col-span-2">Engrain / January 2019 - Present</h5>
-            <p class="col-end-4 col-span-2">Product Manager for SaaS based company. Report to Chief 
+      <div className="divider"></div> 
+        <div className="grid h-200 card bg-base-300 rounded-box place-items-center p-8">
+          <div className="grid grid-cols-3 gap-4">
+            <h2 className="col-span-1 text-slate-500" >- Work Experience</h2>
+            <h3 className="font-bold">Product Manager | Data Integration Manager</h3>
+            <h5 className="col-end-4 col-span-2">Engrain / January 2019 - Present</h5>
+            <p className="col-end-4 col-span-2">Product Manager for SaaS based company. Report to Chief 
             Executive Officer and Chief Technology Officer.</p>
-            <h2 class="col-span-3 text-slate-500" ></h2>
-            <h3 class="col-end-4 col-span-2 font-bold">Senior IT Specialist | IT Project Coordinator</h3>
-            <h5 class="col-end-4 col-span-2">UCOMM LLC / January 2012 - December 2018</h5>
-            <p class="col-end-4 col-span-2">Internal IT staff member for a privately held multifamily housing company 
+            <h2 className="col-span-3 text-slate-500" ></h2>
+            <h3 className="col-end-4 col-span-2 font-bold">Senior IT Specialist | IT Project Coordinator</h3>
+            <h5 className="col-end-4 col-span-2">UCOMM LLC / January 2012 - December 2018</h5>
+            <p className="col-end-4 col-span-2">Internal IT staff member for a privately held multifamily housing company 
             with 200 staff, 27 properties and 3000  leased units. Report to Chief Operating Officer and Chief 
             Accounting Officer. </p>
-            <h2 class="col-span-3 text-slate-500" ></h2>
-            <h2 class="col-span-1 text-slate-500" >- Education</h2>
-            <h3 class="font-bold">Colorado State University Global</h3>
-            <h5 class="col-end-4 col-span-2">2021 - Present</h5>
-            <p class="col-end-4 col-span-2">BS in Computer Science</p>
-            <h2 class="col-span-3 text-slate-500" ></h2>
-            <h3 class="col-end-4 col-span-2 font-bold">Allegheny College</h3>
-            <h5 class="col-end-4 col-span-2">2003 - 2007</h5>
-            <p class="col-end-4 col-span-2">BA in International Studies and Political Science</p>
-            <h2 class="col-span-3 text-slate-500" ></h2>
-            <h2 class="col-span-1 text-slate-500" >- Skills</h2>
+            <h2 className="col-span-3 text-slate-500" ></h2>
+            <h2 className="col-span-1 text-slate-500" >- Education</h2>
+            <h3 className="font-bold">Colorado State University Global</h3>
+            <h5 className="col-end-4 col-span-2">2021 - Present</h5>
+            <p className="col-end-4 col-span-2">BS in Computer Science</p>
+            <h2 className="col-span-3 text-slate-500" ></h2>
+            <h3 className="col-end-4 col-span-2 font-bold">Allegheny College</h3>
+            <h5 className="col-end-4 col-span-2">2003 - 2007</h5>
+            <p className="col-end-4 col-span-2">BA in International Studies and Political Science</p>
+            <h2 className="col-span-3 text-slate-500" ></h2>
+            <h2 className="col-span-1 text-slate-500" >- Skills</h2>
             <p>JavaScript</p>
-            <progress class="progress progress-primary w-56" value="40" max="100"></progress>
-            <h2 class="col-span-1 text-slate-500" ></h2>
+            <progress className="progress progress-primary w-56" value="40" max="100"></progress>
+            <h2 className="col-span-1 text-slate-500" ></h2>
             <p>PHP</p>
-            <progress class="progress progress-secondary w-56" value="70" max="100"></progress>
-            <h2 class="col-span-1 text-slate-500" ></h2>
+            <progress className="progress progress-secondary w-56" value="70" max="100"></progress>
+            <h2 className="col-span-1 text-slate-500" ></h2>
             <p>React</p>
-            <progress class="progress progress-accent w-56" value="40" max="100"></progress>
-            <h2 class="col-span-1 text-slate-500" ></h2>
+            <progress className="progress progress-accent w-56" value="40" max="100"></progress>
+            <h2 className="col-span-1 text-slate-500" ></h2>
             <p>Docker</p>
-            <progress class="progress progress-primary w-56" value="40" max="100"></progress>
-            <h2 class="col-span-1 text-slate-500" ></h2>
+            <progress className="progress progress-primary w-56" value="40" max="100"></progress>
+            <h2 className="col-span-1 text-slate-500" ></h2>
             <p>Git</p>
-            <progress class="progress progress-secondary w-56" value="80" max="100"></progress>
-            <h2 class="col-span-1 text-slate-500" ></h2>
+            <progress className="progress progress-secondary w-56" value="80" max="100"></progress>
+            <h2 className="col-span-1 text-slate-500" ></h2>
             <p>Laravel</p>
-            <progress class="progress progress-accent w-56" value="50" max="100"></progress>
+            <progress className="progress progress-accent w-56" value="50" max="100"></progress>
             
             </div>
           </div>
@@ -74,7 +74,7 @@ export default function Home() {
 
         <footer className="items-center p-4 footer bg-neutral text-neutral-content">
               <div className="flex items-center">
-                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillrule="evenodd" clip-rule="evenodd" className="fill-current">
+                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current">
                   <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
                 </svg> 
                 <p>Copyright © 2021 - All right reserved</p>
