8f6c0f94ce21ef8e3bd24cee7c041355f92b2ca7
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2023-05-19 21:10:57 -0400

    make updates to portfolio.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-// import '../styles/globals.css'
+import "../styles/globals.css";
+import { Cinzel, Arapey, Bebas_Neue } from "next/font/google";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const cinzel = Cinzel({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--font-cinzel",
+});
+
+const arapey = Arapey({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--font-arapey",
+});
+
+const bebasneue = Bebas_Neue({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--font-bebasneue",
+});
+
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<div className={`${cinzel.variable} font-cinzel`}>
+				<div
+					className={`${arapey.variable} font-arapey`}
+				>
+					<div
+						className={`${bebasneue.variable} font-bebasneue`}
+					>
+						<Component {...pageProps} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default MyApp
