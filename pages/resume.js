a095b89ce3fe12430203718a6e32e52c33b7ceaa
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2023-07-20 16:36:08 -0400

    content updates

--- pages/resume.js
+++ pages/resume.js
@@ -8,7 +8,20 @@ export default function Resume() {
 				<Header />
 				<div className="flex flex-col sm:flex-row justify-items-center bg-background">
 					<div className="flex flex-col mt-16 px-12 sm:px-32">
-						<h1 className="font-bebasneue text-2xl border-b-2">
+						<h2 className="font-bebasneue text-2xl border-b-2">
+							Objective
+						</h2>
+						<p className="font-arapey mt-4">
+							Detail-oriented,
+							organized and meticulous
+							employee. Works at fast
+							pace to meet tight
+							deadlines. Enthusiastic
+							team player ready to
+							contribute to company
+							success.
+						</p>
+						<h1 className="font-bebasneue text-2xl border-b-2 mt-8">
 							Work Experience
 						</h1>
 						<h3 className="font-bebasneue text-lg mt-8">
@@ -16,71 +29,134 @@ export default function Resume() {
 							2022 - Present
 						</h3>
 						<p className="font-arapey mt-4">
-							Lorem ipsum dolor sit
-							amet, consectetur
-							adipiscing elit, sed do
-							eiusmod tempor
-							incididunt ut labore et
-							dolore magna aliqua.
-							Erat pellentesque
-							adipiscing commodo elit
-							at imperdiet dui
-							accumsan sit. Est sit
-							amet facilisis magna
-							etiam tempor. Lectus
-							nulla at volutpat diam
-							ut venenatis. Orci ac
-							auctor augue mauris
-							augue. Integer quis
-							auctor elit sed.
+							Saas based company
+							working to help small
+							businesses in the
+							Carribean expand their
+							online presence.
 						</p>
+						<ul className="font-arapey mt-4 list-disc indent-8">
+							<li>
+								Consulted with
+								customers to
+								assess needs and
+								propose optimal
+								solutions.
+							</li>
+							<li>
+								Enhanced
+								operational
+								efficiency and
+								productivity by
+								managing
+								budgets,
+								accounts, and
+								costs.
+							</li>
+							<li>
+								Managed
+								day-to-day
+								business
+								operations.
+							</li>
+						</ul>
+
 						<h3 className="font-bebasneue text-lg mt-8">
 							Product Manager | Data
 							Integrations Manager:
 							January 2019 - June 2022
 						</h3>
 						<p className="font-arapey mt-4">
-							Lorem ipsum dolor sit
-							amet, consectetur
-							adipiscing elit, sed do
-							eiusmod tempor
-							incididunt ut labore et
-							dolore magna aliqua.
-							Erat pellentesque
-							adipiscing commodo elit
-							at imperdiet dui
-							accumsan sit. Est sit
-							amet facilisis magna
-							etiam tempor. Lectus
-							nulla at volutpat diam
-							ut venenatis. Orci ac
-							auctor augue mauris
-							augue. Integer quis
-							auctor elit sed.
+							Product Manager for SaaS
+							based company. Report to
+							Chief Executive Officer
+							and Chief Technology
+							Officer.
 						</p>
+						<ul className="font-arapey mt-4 list-disc indent-8">
+							<li>
+								Coordinated
+								project planning
+								and execution
+								with team
+								members and team
+								leads.
+							</li>
+							<li>
+								Reviewed sales,
+								customer
+								concerns, and
+								new
+								opportunities to
+								drive business
+								strategy at
+								weekly planning
+								sessions.
+							</li>
+							<li>
+								Prioritized
+								roadmap in order
+								to achieve
+								product goals
+								and metrics.
+							</li>
+							<li>
+								Collaborated
+								with sales,
+								marketing, and
+								support teams to
+								launch products
+								on time and
+								within budget.
+							</li>
+						</ul>
+
 						<h3 className="font-bebasneue text-lg mt-8">
 							Senior IT Specialist:
-							January 2012 - December 2018
+							January 2012 - December
+							2018
 						</h3>
 						<p className="font-arapey mt-4">
-							Lorem ipsum dolor sit
-							amet, consectetur
-							adipiscing elit, sed do
-							eiusmod tempor
-							incididunt ut labore et
-							dolore magna aliqua.
-							Erat pellentesque
-							adipiscing commodo elit
-							at imperdiet dui
-							accumsan sit. Est sit
-							amet facilisis magna
-							etiam tempor. Lectus
-							nulla at volutpat diam
-							ut venenatis. Orci ac
-							auctor augue mauris
-							augue. Integer quis
-							auctor elit sed.
+							Internal IT staff member
+							for a privately held
+							multifamily housing
+							company with 200 staff,
+							27 properties and 3000
+							leased units. Report to
+							Chief Operating Officer
+							and Chief Accounting
+							Officer.
 						</p>
+						<ul className="font-arapey mt-4 list-disc indent-8">
+							<li>
+								Organized system
+								operating
+								procedures to
+								strengthen
+								controls.
+							</li>
+							<li>
+								Improved systems
+								with addition of
+								new features and
+								infrastructure.
+							</li>
+							<li>
+								Participated in
+								internal audits
+								regarding
+								quality
+								management
+								system.
+							</li>
+							<li>
+								Educated
+								stakeholders on
+								data protection
+								tactics to
+								reduce breaches.
+							</li>
+						</ul>
 					</div>
 					<div className="flex flex-col mt-16 px-12 sm:px-32">
 						<h1 className="font-bebasneue text-2xl border-b-2">
@@ -137,27 +213,13 @@ export default function Resume() {
 									Science
 								</h3>
 								<p className="font-arapey mt-4">
-									ipsum
-									dolor
-									sit
-									amet,Lorem
-									ipsum
-									dolor
-									sit
-									amet,
-									consectetur
-									adipiscing
-									elit,
-									sed do
-									eiusmod
-									tempor
-									incididunt
-									ut
-									labore
-									et
-									dolore
-									magna
-									aliqua.
+									Colorado
+									State
+									University
+									Global
+									Campus -
+									Aurora,
+									CO
 								</p>
 							</div>
 							<div className="flex flex-col mt-8 gap-4">
@@ -170,27 +232,11 @@ export default function Resume() {
 									Science
 								</h3>
 								<p className="font-arapey mt-4">
-									ipsum
-									dolor
-									sit
-									amet,Lorem
-									ipsum
-									dolor
-									sit
-									amet,
-									consectetur
-									adipiscing
-									elit,
-									sed do
-									eiusmod
-									tempor
-									incididunt
-									ut
-									labore
-									et
-									dolore
-									magna
-									aliqua.
+									Allegheny
+									College
+									-
+									Meadville,
+									PA
 								</p>
 							</div>
 						</div>
