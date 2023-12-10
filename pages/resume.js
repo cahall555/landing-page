58937ce73868ec2ad16d608c3a9ed85adc04b9cc
Courtney Elsner <courtneyelsner@pop-os.localdomain>
2023-12-10 14:16:37 -0400

    update resume.

--- pages/resume.js
+++ pages/resume.js
@@ -6,66 +6,89 @@ export default function Resume() {
 		<>
 			<div className="h-screen w-full mx-auto bg-background">
 				<Header />
-				<div className="flex flex-col sm:flex-row justify-items-center bg-background">
-					<div className="flex flex-col mt-16 px-12 sm:px-32">
+				<div className="flex flex-col justify-center text-center pb-8 px-12">
+					<h1 className="font-bebasneue text-6xl mt-8">
+						Software Developer
+					</h1>
+					<h3 className="font-arapey text-lg sm:text-xl sm:font-semibold">
+						3 years Software Development
+					</h3>
+					<h3 className="font-arapey text-lg sm:text-xl sm:font-semibold">
+						8+ years of experience in IT
+						Management | Project Management
+						| Accounting Controls & Auditing
+					</h3>
+				</div>
+				<div className="flex flex-col sm:flex-row justify-items-center bg-background pb-8">
+					<div className="flex flex-col mt-8 px-12 sm:px-32">
 						<h2 className="font-bebasneue text-2xl border-b-2">
 							Objective
 						</h2>
 						<p className="font-arapey mt-4">
 							Detail-oriented,
 							organized and meticulous
-							employee. Works at fast
-							pace to meet tight
-							deadlines. Enthusiastic
-							team player ready to
-							contribute to company
-							success.
+							full-stack developer.
+							Works at a fast pace to
+							meet tight deadlines.
+							Enthusiastic team player
+							ready to contribute to
+							company success.
 						</p>
 						<h1 className="font-bebasneue text-2xl border-b-2 mt-8">
 							Work Experience
 						</h1>
 						<h3 className="font-bebasneue text-lg mt-8">
-							Developer/Owner: June
-							2022 - Present
+							CONNECTIVE HORIZONS
 						</h3>
+						<div className="flex flex-col md:flex-row justify-between font-bebasneue text-lg">
+							<h3>
+								Software
+								Developer |
+								Owner
+							</h3>
+							<h3>2023 - Present</h3>
+						</div>
 						<p className="font-arapey mt-4">
-							Saas based company
+							SaaS based company
 							working to help small
 							businesses in the
 							Caribbean expand their
 							online presence.
 						</p>
-						<ul className="font-arapey mt-4 list-disc indent-8">
-							<li>
-								Consulted with
-								customers to
-								assess needs and
-								propose optimal
-								solutions.
-							</li>
-							<li>
-								Enhanced
-								operational
-								efficiency and
-								productivity by
-								managing
-								budgets,
-								accounts, and
-								costs.
-							</li>
-							<li>
-								Managed
-								day-to-day
-								business
-								operations.
-							</li>
-						</ul>
-
+						<div className="ml-8">
+							<ul className="font-arapey mt-4 list-disc list-outside">
+								<li>
+									Consulted
+									with
+									customers
+									to
+									assess
+									needs
+									and
+									propose
+									optimal
+									solutions.
+								</li>
+								<li>
+									Managed
+									day-to-day
+									business
+									operations.
+								</li>
+							</ul>
+						</div>
 						<h3 className="font-bebasneue text-lg mt-8">
-							Product Manager | Data
-							Integrations Manager:
-							January 2019 - June 2022
+							ENGRAIN
 						</h3>
+						<div className="flex flex-col md:flex-row justify-between font-bebasneue text-lg">
+							<h3>
+								Product Manager
+								| Data
+								Integrations
+								Manager
+							</h3>
+							<h3>2019 - 2022</h3>
+						</div>
 						<p className="font-arapey mt-4">
 							Product Manager for SaaS
 							based company. Report to
@@ -73,53 +96,114 @@ export default function Resume() {
 							and Chief Technology
 							Officer.
 						</p>
-						<ul className="font-arapey mt-4 list-disc indent-8">
-							<li>
-								Coordinated
-								project planning
-								and execution
-								with team
-								members and team
-								leads.
-							</li>
-							<li>
-								Reviewed sales,
-								customer
-								concerns, and
-								new
-								opportunities to
-								drive business
-								strategy at
-								weekly planning
-								sessions.
-							</li>
-							<li>
-								Prioritized
-								roadmap in order
-								to achieve
-								product goals
-								and metrics.
-							</li>
-							<li>
-								Collaborated
-								with sales,
-								marketing, and
-								support teams to
-								launch products
-								on time and
-								within budget.
-							</li>
-						</ul>
-
+						<div className="ml-8">
+							<ul className="font-arapey mt-4 list-disc list-outside">
+								<li>
+									Crafted
+									software
+									with PHP
+									and
+									Laravel
+									for
+									retrieving
+									and
+									handling
+									data
+									from
+									APIs to
+									aid
+									account
+									managers
+									and
+									support
+									teams
+									with
+									troubleshooting.
+								</li>
+								<li>
+									Created
+									ETL
+									processes
+									to
+									extract
+									pricing
+									and
+									availability
+									data
+									from
+									external
+									APIs and
+									load
+									information
+									into
+									Engrain
+									SightMaps,
+									leveraging
+									Kubernetes,
+									Laravel,
+									PHP and
+									scheduled
+									cron
+									jobs for
+									implementation.
+								</li>
+								<li>
+									Researched
+									third
+									party
+									APIs,
+									create
+									specification
+									documents,
+									and work
+									with
+									software
+									developers
+									to
+									develop
+									new
+									integration
+									features.
+								</li>
+								<li>
+									Coordinated
+									project
+									planning
+									and
+									execution
+									with
+									team
+									members
+									and team
+									leads.
+								</li>
+								<li>
+									Prioritized
+									roadmap
+									in order
+									to
+									achieve
+									product
+									goals
+									and
+									metrics.
+								</li>
+							</ul>
+						</div>
 						<h3 className="font-bebasneue text-lg mt-8">
-							Senior IT Specialist:
-							January 2012 - December
-							2018
+							UCOMM, LLC
 						</h3>
+						<div className="flex flex-col md:flex-row justify-between font-bebasneue text-lg">
+							<h3>
+								Senior IT
+								Specialist
+							</h3>
+							<h3>2012 - 2018</h3>
+						</div>
 						<p className="font-arapey mt-4">
-							Internal IT staff member
-							for a privately held
-							multifamily housing
+							Sole internal IT staff
+							member for a privately
+							held multifamily housing
 							company with 200 staff,
 							27 properties and 3000
 							leased units. Report to
@@ -127,39 +211,337 @@ export default function Resume() {
 							and Chief Accounting
 							Officer.
 						</p>
-						<ul className="font-arapey mt-4 list-disc indent-8">
-							<li>
-								Organized system
-								operating
-								procedures to
-								strengthen
-								controls.
-							</li>
-							<li>
-								Improved systems
-								with addition of
-								new features and
-								infrastructure.
-							</li>
-							<li>
-								Participated in
-								internal audits
-								regarding
-								quality
-								management
-								system.
-							</li>
-							<li>
-								Educated
-								stakeholders on
-								data protection
-								tactics to
-								reduce breaches.
-							</li>
-						</ul>
+						<div className="ml-8">
+							<ul className="font-arapey mt-4 list-disc list-outside">
+								<li>
+									Oversee
+									outsourced
+									technology
+									solution
+									providers
+									including
+									PC
+									hardware,
+									SaaS,
+									VoIP
+									telecommunications,
+									satellite
+									internet
+									service,
+									network
+									management,
+									and ISPs
+									for 27
+									locations
+									nationwide.
+								</li>
+								<li>
+									Serve as
+									Network
+									Administrator,
+									assuring
+									security
+									and
+									uptime
+									of 65
+									LANs,
+									200
+									workstations,
+									and
+									numerous
+									peripheral
+									devices.
+								</li>
+								<li>
+									Anticipate,
+									test,
+									and
+									address
+									security
+									concerns
+									to
+									protect
+									and
+									maintain
+									network
+									integrity.
+									Create
+									policies/set
+									guidelines
+									for
+									vendor
+									network
+									access,
+									evaluate
+									network
+									peripherals
+									for
+									known
+									security
+									vulnerabilities,
+									and
+									define
+									geographic
+									location
+									of
+									network
+									equipment.
+								</li>
+								<li>
+									Direct
+									system
+									migrations,
+									including
+									Yardi to
+									RealPage
+									Leasing
+									and
+									Rents at
+									18
+									locations,
+									and
+									email
+									servers
+									from
+									on-premises
+									Microsoft
+									Exchange
+									Server
+									to
+									Microsoft
+									Office
+									365.
+								</li>
+								<li>
+									Initiate
+									and
+									track{" "}
+									{">"}$2M
+									in wire
+									payments
+									monthly.
+									Support
+									treasury
+									operations
+									as
+									needed.
+									Create
+									IT
+									expense
+									projection
+									and
+									track
+									performance
+									to
+									budget.
+								</li>
+								<li>
+									Led
+									planning
+									and
+									implementation
+									of
+									seamless
+									technology
+									and
+									accounting
+									integrations
+									for 13
+									property
+									acquisitions,
+									assuring
+									positive
+									tenant
+									relations
+									and
+									operational
+									continuity
+									during
+									transition
+									phase.
+								</li>
+								<li>
+									Developed
+									procedures
+									to
+									complement
+									migration
+									to a new
+									accounting
+									system.
+								</li>
+							</ul>
+						</div>
+						<h3 className="font-bebasneue text-lg mt-8">
+							GRAEBEL COMPANIES, INC.
+						</h3>
+						<div className="flex flex-col md:flex-row justify-between font-bebasneue text-lg">
+							<h3>
+								Accounting
+								Specialist |
+								Internal Auditor
+							</h3>
+							<h3>2008 - 2011</h3>
+						</div>
+						<p className="font-arapey mt-4">
+							Supported a team of 75
+							consultants in serving a
+							portfolio of up to 800
+							clients in expat work
+							assignments worldwide,
+							processing a large
+							volume of electronic
+							banking / ACH transfers,{" "}
+							{">"}$2M per day in
+							housing and moving
+							expenses.
+						</p>
+						<div className="ml-8">
+							<ul className="font-arapey mt-4 list-disc list-outside">
+								<li>
+									Initiated
+									a
+									program
+									to
+									schedule
+									and
+									consolidate
+									vendor
+									payments,
+									significantly
+									improving
+									cash
+									flow and
+									reducing
+									wire
+									fees by
+									$25K
+									annually.{" "}
+								</li>
+								<li>
+									Identified
+									need to
+									tighten
+									communication
+									processes
+									between
+									International
+									Relocation
+									Services,
+									Operations,
+									and
+									Finance
+									teams.
+									Developed
+									a
+									SharePoint
+									site to
+									serve as
+									an ad
+									hoc
+									trouble
+									ticketing
+									system,
+									creating
+									a
+									centralized
+									point of
+									communication,
+									eliminating
+									email
+									chains
+									with a
+									live
+									discussion
+									option.{" "}
+								</li>
+								<li>
+									Conducted
+									live and
+									WebEx
+									training
+									for{" "}
+									{">"}100
+									Operations
+									and
+									Finance
+									staff in
+									Denver,
+									Atlanta,
+									Singapore,
+									and
+									Prague
+									on
+									effective
+									domestic
+									and
+									international
+									EFT
+									transactions.{" "}
+								</li>
+								<li>
+									Supported
+									complex
+									internal
+									audit
+									processes
+									for
+									international
+									real
+									estate
+									holdings.{" "}
+								</li>
+							</ul>
+						</div>
 					</div>
-					<div className="flex flex-col mt-16 px-12 sm:px-32">
-						<h1 className="font-bebasneue text-2xl border-b-2">
+
+					<div className="flex flex-col mt-8 px-12 sm:px-32">
+						<div>
+							<h1 className="font-bebasneue text-2xl border-b-2">
+								Education
+							</h1>
+							<div className="flex flex-col mt-8">
+								<h3 className="font-bebasneue text-lg">
+									BS in
+									Computer
+									Science
+									- 2023
+								</h3>
+								<p className="font-arapey">
+									Magna
+									Cum
+									Laude
+								</p>
+								<p className="font-arapey mt-4">
+									Colorado
+									State
+									University
+									Global
+									Campus -
+									Aurora,
+									CO
+								</p>
+							</div>
+							<div className="flex flex-col mt-8 gap-4">
+								<h3 className="font-bebasneue text-lg">
+									BA in
+									International
+									Studies
+									&
+									Political
+									Science
+									- 2007
+								</h3>
+								<p className="font-arapey mt-4">
+									Allegheny
+									College
+									-
+									Meadville,
+									PA
+								</p>
+							</div>
+						</div>
+						<h1 className="font-bebasneue text-2xl border-b-2 mt-12">
 							Skills
 						</h1>
 						<div className="flex flex-row mt-8 gap-4">
@@ -202,42 +584,21 @@ export default function Resume() {
 								<div className="h-4 w-40 bg-light-green rounded-full"></div>
 							</div>
 						</div>
-						<div>
-							<h1 className="font-bebasneue text-2xl border-b-2 mt-12">
-								Education
-							</h1>
-							<div className="flex flex-col mt-8 gap-4">
-								<h3 className="font-bebasneue text-lg">
-									BS in
-									Computer
-									Science
-								</h3>
-								<p className="font-arapey mt-4">
-									Colorado
-									State
-									University
-									Global
-									Campus -
-									Aurora,
-									CO
-								</p>
+						<div className="flex flex-row mt-8 gap-4">
+							<h3 className="font-bebasneue text-lg">
+								Python
+							</h3>
+							<div className="h-4 w-48 bg-gray rounded-full">
+								<div className="h-4 w-20 bg-rose rounded-full"></div>
 							</div>
-							<div className="flex flex-col mt-8 gap-4">
-								<h3 className="font-bebasneue text-lg">
-									BA in
-									International
-									Studies
-									&
-									Political
-									Science
-								</h3>
-								<p className="font-arapey mt-4">
-									Allegheny
-									College
-									-
-									Meadville,
-									PA
-								</p>
+						</div>
+
+						<div className="flex flex-row mt-8 gap-4">
+							<h3 className="font-bebasneue text-lg">
+								Jira
+							</h3>
+							<div className="h-4 w-48 bg-gray rounded-full">
+								<div className="h-4 w-40 bg-dark-green rounded-full"></div>
 							</div>
 						</div>
 					</div>
